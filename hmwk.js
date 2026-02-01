372bc91214b326b8db60009ed16c8dabf75e2ac1
tguniava <33898670+tguniava@users.noreply.github.com>
2026-02-01 16:59:37 +0400

    added ex. 2

--- hmwk.js
+++ hmwk.js
@@ -1,4 +1,11 @@
 console.log ('this is the correct file')
-let x=87;
+function ex1 () {
+    let x=87;
 let y=65;
 console.log (x+y);
+}
+function ex2 () {
+let a=23; 
+let a=b;
+console.log (c=a+b);
+}
